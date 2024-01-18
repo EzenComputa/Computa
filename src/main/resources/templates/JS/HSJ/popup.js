5f506da7bb088d59c5bd6ae997f3c55ff67241a8
HSJ0525 <jumbejombe@gmail.com>
2024-01-18 16:55:20 +0900

    HSJ 폴더 생성 및 HTML+CSS+JS+IMG 추가

---
+++ src/main/resources/templates/JS/HSJ/popup.js
@@ -0,0 +1,6 @@
+function showRepPop() {
+    window.open("report_popup.html", "a", "width=600, height=500, left=100, top=50");
+}
+function showDelPop(){
+    window.open("del_popup.html", "a", "width=600, height=500, left=500, top=300");
+}
