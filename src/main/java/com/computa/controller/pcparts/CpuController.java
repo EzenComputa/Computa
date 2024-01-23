d3d005c4066ecaf7c8b3788e12d2bf440ecb797e
zinc030 <ayeon.oh@yonsei.ac.kr>
2024-01-23 12:23:05 +0900

    부품별 컨트롤러 업데이트

--- src/main/java/com/computa/controller/pcparts/CpuController.java
+++ src/main/java/com/computa/controller/pcparts/CpuController.java
@@ -1,4 +1,4 @@
-package com.computa.controller;
+package com.computa.controller.pcparts;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
