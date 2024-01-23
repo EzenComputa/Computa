ee0ea5be0833285ce833a78fbcb258742d15b400
zinc030 <ayeon.oh@yonsei.ac.kr>
2024-01-23 12:35:32 +0900

    Cpu 컨트롤러 업데이트

--- src/main/java/com/computa/controller/pcparts/CpuController.java
+++ src/main/java/com/computa/controller/pcparts/CpuController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.computa.entity.pcparts.Cpu;
 import com.computa.service.pcparts.CpuService;
 
 @Controller
