a16dbd0cb2b4bc5302933254657752a3715150b6
zinc030 <ayeon.oh@yonsei.ac.kr>
2024-01-23 12:28:58 +0900

    부품별 서비스 추가

---
+++ src/main/java/com/computa/service/pcparts/GpuService.java
@@ -0,0 +1,9 @@
+package com.computa.service.pcparts;
+
+import java.util.List;
+
+import com.computa.entity.pcparts.Gpu;
+
+public interface GpuService extends ProductService<Gpu>{
+    List<Gpu> findAll();
+}
