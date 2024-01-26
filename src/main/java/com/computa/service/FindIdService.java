11f22bcda09fae803c6777a5d1ee30b5ed20004f
goodwoodmin <139572509+goodwoodmin@users.noreply.github.com>
2024-01-26 12:49:40 +0900

    CMH / service / FindIdService.java 추가

---
+++ src/main/java/com/computa/service/FindIdService.java
@@ -0,0 +1,8 @@
+// package com.computa.service;
+
+// import com.computa.entity.User;
+
+// public interface FindIdService {
+//     User findUserByUsername(String username);
+//     User findUserBySsid(String ssid);
+// }
