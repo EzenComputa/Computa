c5e004ad71a475ca040b575dbdcf14fb6d41e392
goodwoodmin <light3326@naver.com>
2024-02-02 19:00:12 +0900

    findid관련 html, service, repo, servieImpl, control 생성

--- src/main/java/com/computa/config/SpringSecurity.java
+++ src/main/java/com/computa/config/SpringSecurity.java
@@ -18,51 +18,67 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class SpringSecurity {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+        @Autowired
+        private UserDetailsService userDetailsService;
 
-    @Bean
-    public static PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        public static PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        
-        http.csrf().disable()
-                .authorizeHttpRequests((authorize) ->
-                        authorize.requestMatchers("/register/**").permitAll() // anybody can access /register/**
-                                .requestMatchers("/login","/index","/","","/product/**", "/product_details","/addProduct/**","/saveProduct/**","/find_user", "/findpw/**","/newpw/**").permitAll() // anybody can access 
-                                .requestMatchers("/wishlist/**", "/product/**", "/profile", "/user/**", "/postDetails/**","/writePost/**", "/savePost", "/saveComment", "/saveProductComment", "/uploadImage", "/images/**","/uploadCommentImage", "/myMain", "/likeProd", "/usedMyshop", "/myQnaList", "/myTeabag").hasRole("USER") // only users with USER role can access
-                                .requestMatchers("/manage_users").hasRole("ADMIN") // only users with ADMIN role can access /users
-                ).formLogin(
-                        form -> form
-                                .loginPage("/login")
-                                .loginProcessingUrl("/login") // url for login form's POST request 
-                                .defaultSuccessUrl("/index", true) // If user successfully logs in, redirect to /index
-                                .permitAll() // This is necessary because users need to be able to access these URLs before they're authenticated
-                ).logout(
-                        logout -> logout
-                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                                .permitAll()
-                );
-        return http.build(); // The filterChain method returns a SecurityFilterChain object. 
-    }
-    
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth                                            // In this project, loadUserByUsername() method has a custom implementation
-                .userDetailsService(userDetailsService) // Spring automatically injects the CustomUserDetailsService bean herec
-                .passwordEncoder(passwordEncoder());
-        
-    }
+                http.csrf().disable()
+                                .authorizeHttpRequests((authorize) -> authorize.requestMatchers("/register/**")
+                                                .permitAll() // anybody can access /register/**
+                                                .requestMatchers("/login", "/index", "/", "", "/product/**",
+                                                                "/product_details", "/addProduct/**", "/saveProduct/**",
+                                                                "/find_user", "/findpw/**", "/newpw/**", "/findid/**")
+                                                .permitAll() // anybody can access
+                                                .requestMatchers("/wishlist/**", "/product/**", "/profile", "/user/**",
+                                                                "/postDetails/**", "/writePost/**", "/savePost",
+                                                                "/saveComment", "/saveProductComment", "/uploadImage",
+                                                                "/images/**", "/uploadCommentImage", "/myMain",
+                                                                "/likeProd", "/usedMyshop", "/myQnaList", "/myTeabag")
+                                                .hasRole("USER") // only users with USER role can access
+                                                .requestMatchers("/manage_users").hasRole("ADMIN") // only users with
+                                                                                                   // ADMIN role can
+                                                                                                   // access /users
+                                ).formLogin(
+                                                form -> form
+                                                                .loginPage("/login")
+                                                                .loginProcessingUrl("/login") // url for login form's
+                                                                                              // POST request
+                                                                .defaultSuccessUrl("/index", true) // If user
+                                                                                                   // successfully logs
+                                                                                                   // in, redirect to
+                                                                                                   // /index
+                                                                .permitAll() // This is necessary because users need to
+                                                                             // be able to access these URLs before
+                                                                             // they're authenticated
+                                ).logout(
+                                                logout -> logout
+                                                                .logoutRequestMatcher(
+                                                                                new AntPathRequestMatcher("/logout"))
+                                                                .permitAll());
+                return http.build(); // The filterChain method returns a SecurityFilterChain object.
+        }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring()
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
-                .requestMatchers("/javax.faces.resource/**");
-    }
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+                auth // In this project, loadUserByUsername() method has a custom implementation
+                                .userDetailsService(userDetailsService) // Spring automatically injects the
+                                                                        // CustomUserDetailsService bean herec
+                                .passwordEncoder(passwordEncoder());
+
+        }
+
+        @Bean
+        public WebSecurityCustomizer webSecurityCustomizer() {
+                return (web) -> web.ignoring()
+                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                                .requestMatchers("/javax.faces.resource/**");
+        }
 
 }
--- src/main/java/com/computa/controller/FindIdController.java
+++ src/main/java/com/computa/controller/FindIdController.java
@@ -1,51 +1,34 @@
-// package com.computa.controller;
+package com.computa.controller;
 
-// import org.springframework.stereotype.Controller;
-// import org.springframework.ui.Model;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-// import com.computa.entity.User;
-// import com.computa.service.UserService;
+import com.computa.service.FindIdService;
 
-// @Controller
-// public class FindIdController {
+import org.springframework.web.bind.annotation.PostMapping;
 
-//     private UserService userService;
+@Controller
+public class FindIdController {
 
-//     public FindIdController(FindIdService findIdService, UserService userService) {
-//         this.findIdService = findIdService;
-//         this.userService = userService;
-//     }
+    private FindIdService findidService;
 
-//     @GetMapping("/findId")
-//     public String getFindId(Model model, @RequestParam String username, @RequestParam String ssid) {
-//         // UserService에 있는 username, ssid 정보
-//         User usernameInfo = userService.findUserByUsername(username);
-//         User ssidInfo = userService.findUserBySsid(ssid);
+    public FindIdController(FindIdService findIdService) {
+        this.findidService = findIdService;
 
-//         // UserTable의 id와 ssid 비교 
-//         if (usernameInfo != null && ssidInfo != null && usernameInfo.equals(ssidInfo)) {
-//             // Add data to the model if needed
-//             model.addAttribute("username", usernameInfo.getUsername());
-//             model.addAttribute("ssid", ssidInfo.getSsid());
+    }
 
-//             return "show_id"; // "show_id"가 올바른 뷰 이름인지 확인하세요.
-//         } else {
-//             // username 또는 ssid를 찾지 못한 경우 처리
-//             return "error_page"; // 이에 대한 오류 페이지를 만들어 사용하세요.
-//         }
-//     }
+    @GetMapping("/findid")
+    public String getFindIdPage() {
+        return "findid";
+    }
 
-//     @PostMapping("/findpw/btn")
-//     public String postFindId(String username, String userSsid) {
-//         // 매개변수로 받은 userid와 userssid(postFindPw)를 받아 userService의 Username을 찾음
-//         User checkUsername = userService.findUserByUsername(username);
-//         User checkUserSsid = userService.findUserBySsid(userSsid);
+    @PostMapping("/findid/submit") // findid.html line:62
+    public String postFindID(@RequestParam("name") String name, Model model) { // findid.html line:62
+        String ID = findidService.checkName(name);
+        model.addAttribute("message", ID);
+        return "find_user_id";
+    }
 
-//         // 추가로 요구되는 로직을 기반으로 필요한 로직 추가 가능
-
-//         return "redirect:/findid";
-//     }
-// }
+}
