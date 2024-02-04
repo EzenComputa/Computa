45167d7c6b2a1e8912b578067fe91ccabd4fcb7a
HSJ0525 <jumbejombe@gmail.com>
2024-02-04 17:03:26 +0900

    pcpart entity 생성, pcpart html 수정

--- src/main/java/com/computa/controller/myMainController.java
+++ src/main/java/com/computa/controller/myMainController.java
@@ -1,38 +1,38 @@
-// package com.computa.controller;
+package com.computa.controller;
 
-// import org.springframework.stereotype.Controller;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
-// @Controller
-// public class myMainController {
-//     @GetMapping("/myMain")
-//     public String getmyMain() {
+@Controller
+public class myMainController {
+    @GetMapping("/myMain")
+    public String getmyMain() {
 
-//         return "my_main";
-//     }
+        return "my_main";
+    }
 
-//     @GetMapping("/likeProd")
-//     public String getlikeProd() {
+    @GetMapping("/likeProd")
+    public String getlikeProd() {
 
-//         return "like_prod";
-//     }
+        return "like_prod";
+    }
  
-//     @GetMapping("/usedMyshop")
-//     public String getusedMyshop() {
-//         return "used_myshop";
-//     }
+    @GetMapping("/usedMyshop")
+    public String getusedMyshop() {
+        return "used_myshop";
+    }
 
-//     @GetMapping("/myQnaList")
-//     public String getMethodName() {
-//         return "my_qna_list";
-//     }
+    @GetMapping("/myQnaList")
+    public String getMethodName() {
+        return "my_qna_list";
+    }
 
-//     @GetMapping("/myTeabag")
-//     public String getMyteabag() {
-//         return "estimate";
-//     }
+    @GetMapping("/myTeabag")
+    public String getMyteabag() {
+        return "estimate";
+    }
     
     
-// }
+}
--- src/main/java/com/computa/entity/pcparts/Gpu.java
+++ src/main/java/com/computa/entity/pcparts/Gpu.java
@@ -20,4 +20,67 @@ public class Gpu extends Product{
     @Column(name = "vram")
     @Digits(fraction = 0, integer = 2)
     private Integer vram;
+
+    @Column(name = "chipCompany")
+    private String chipCompany;
+
+    @Column(name = "kinds")
+    private String kinds;
+
+    @Column(name = "kudaprocess")
+    private Integer kudaprocess;
+
+    @Column(name = "chipset")
+    private String chipset;
+
+    @Column(name = "interfaceGpu")
+    private String interfaceGpu;
+
+    @Column(name = "memKind")
+    private String memKind;
+
+    @Column(name = "memBus")
+    private Integer memBus;
+
+    @Column(name = "memSize")
+    private Integer memSize;
+
+    @Column(name = "maxMoniter")
+    private Integer maxMoniter;
+
+    @Column(name = "displayport")
+    private Integer displayport;
+
+    @Column(name = "zerofan")
+    private boolean zerofan;
+
+    @Column(name = "disp8K")
+    private boolean disp8k;
+
+    @Column(name = "disp4K")
+    private boolean disp4K;
+
+    @Column(name = "HDR")
+    private boolean HDR;
+
+    @Column(name = "HDCP")
+    private boolean HDCP;
+
+    @Column(name = "maxpower")
+    private Integer maxpower;
+
+    @Column(name = "protectheat")
+    private boolean protectheat;
+
+    @Column(name = "heatpipe")
+    private boolean heatpipe;
+
+    @Column(name = "fancooler")
+    private boolean fancooler;
+
+    @Column(name = "fancount")
+    private Integer fancount;
+
+    @Column(name = "width")
+    private Integer width;
 }
