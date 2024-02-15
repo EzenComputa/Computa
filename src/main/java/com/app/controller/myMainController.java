3c0838af8c4c625d97b11523b3f9860188123d21
hyjuu <dudwn7648@naver.com>
2024-02-15 13:29:29 +0900

    마이페이지 닉네임, 유저아이디 가져오기

--- src/main/java/com/app/controller/myMainController.java
+++ src/main/java/com/app/controller/myMainController.java
@@ -1,14 +1,45 @@
 package com.app.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.app.entity.User;
+import com.app.service.UserService;
+import com.app.serviceImpl.UserServiceImpl;
 
 
 @Controller
 public class myMainController {
+
+    private UserService userService;
+
+    @Autowired
+    public myMainController(UserService userService){
+        this.userService = userService;
+    }
+
     @GetMapping("/myMain")
-    public String getmyMain() {
+    public String getmyMain(Model model, @AuthenticationPrincipal UserDetails currentUser) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String currentUsername = authentication.getName(); // 현재 로그인한 유저네임을 가져옴
+        User currentUserInfo = userService.findUserByUsername(currentUsername); // 현재 로그인한 사용자의 정보를 데이터베이스에서 찾아옴
+        // 유저 정보를 가져와서 모델에 추가
+        String currentNickname = currentUserInfo.getNickname(); // 현재 로그인한 사용자의 닉네임을 가져옴
+        String currentName = currentUserInfo.getUsername(); // 현재 로그인한 사용자의 이름을 가져옴
+        model.addAttribute("nickname", currentNickname);
+        model.addAttribute("name", currentName);
+
 
         return "my_main";
     }
